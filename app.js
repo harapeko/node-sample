505c39bcd9c90e73c45e9f97847c53f168ff8094
harapeko <tea.lile.tea@gmail.com>
2017-02-28 01:04:13 +0900

    ランダムにページの内容が変わるようにしてみた

--- app.js
+++ app.js
@@ -8,13 +8,21 @@ console.log('Server running!');
 
 // リクエスト の 処理
 function doRequest(req, res) {
+    var number = Math.floor(Math.random() * 3);
+
     fs.readFile(
         './hello.html', 'UTF-8',
         function(err, data){
+            var title = ["ページA", "ページB", "ページC"];
+            var content = ["コンテンツA", "コンテンツB", "コンテンツC"];
+
+            var data2 = data.replace(/@title@/g, title[number])
+                            .replace(/@content@/g, content[number]);
+
             res.writeHead(200, {
                 'Content-Type': 'text/html'}
             );
-            res.write(data);
+            res.write(data2);
             res.end();
         }
     );
