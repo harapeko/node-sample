39055fe9ab1e6d485f06abbfe9e38abbc3b090d3
harapeko <tea.lile.tea@gmail.com>
2017-02-28 02:33:29 +0900

    テンプレートファイルを使うようにしてみた

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var express = require('express');
+var ejs = require("ejs");
+
 var app = express();
 
+app.engine('ejs', ejs.renderFile);
+
 app.get('/', function(req, res){
-    res.send('Hello Japari Park!!');
+    res.render('test.ejs', {
+        title: "Test Page",
+        content: "this is test."
+    });
 });
 
 var server = app.listen(process.env.PORT || 3000, function(){
